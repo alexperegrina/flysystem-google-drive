cb56440a84998206da17723bd9ee03c1d1acf8d4
Alex Peregrina <alex.peregrina@nemuru.com>
2021-01-18 18:28:05 +0100

    Init GoogleDriveAdapter

---
+++ tests/GoogleDriveAdapterTest.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\AlexPeregrina\Flysystem\GoogleDrive;
+
+use League\Flysystem\AdapterTestUtilities\FilesystemAdapterTestCase;
+use League\Flysystem\FilesystemAdapter;
+
+class GoogleDriveAdapterTest extends FilesystemAdapterTestCase
+{
+    protected static function createFilesystemAdapter(): FilesystemAdapter
+    {
+        // TODO: Implement createFilesystemAdapter() method.
+    }
+}
